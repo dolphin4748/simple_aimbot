61cef12f45006ffef1f0d1072a76bcc57f6cb71b
dolphin4748 <151792207+dolphin4748@users.noreply.github.com>
2024-08-05 19:55:39 -0300

    Add files via upload

---
+++ aimbooster.py
@@ -0,0 +1,35 @@
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import win32api, win32con
+
+time.sleep(2)
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+
+#Color of center: (255, 219, 195)
+
+while keyboard.is_pressed('q') == False:
+    flag = 0
+    pic = pyautogui.screenshot(region=(425, 175, 820, 410))
+
+    width, height = pic.size
+
+    for x in range(0, width, 5):
+        for y in range(0, height, 5):
+
+            r, g, b = pic.getpixel((x, y))
+
+            if b >= 190 and r >= 190 and g >=190:
+                flag = 1
+                click(x+425, y+175)
+                time.sleep(0.05)
+                break
+
+        if flag == 1:
+            break
